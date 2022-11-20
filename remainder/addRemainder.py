8ce91f9b0c6d8cc52c31e0edcaf4e74e6519b76f
Adithya B <1999adithya@gmail.com>
2022-11-20 10:48:15 +0530

    Initial Commit

---
+++ remainder/addRemainder.py
@@ -0,0 +1,29 @@
+import json
+import os
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+dynamodb = boto3.resource('dynamodb')
+dbName = os.environ['ddbName']
+table = dynamodb.Table(dbName)
+
+def handler(event, context):
+    logger.info(event)
+    body = json.loads(event['body'])
+    logger.info(body)
+
+    item = {
+        'user': body['user'],
+        'ttl': body['ttl'],
+        'owner': body['owner']
+    }
+
+    table.put_item(Item=item)
+    logger.info("Record inserted into the DB")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(item)
+    }  
